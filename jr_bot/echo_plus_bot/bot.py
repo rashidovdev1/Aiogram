0fbb73e8f1a3de0eec07a3736395b8a2e18183a5
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-23 23:53:00 +0500

    echo plus

--- jr_bot/echo_plus_bot/bot.py
+++ jr_bot/echo_plus_bot/bot.py
@@ -4,18 +4,28 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 import asyncio
 
-BOT_TOKEN = "TOKEN"
+BOT_TOKEN = "8470618802:AAGK1g0j4j4PI3KO3DVjR9nk6LEIbK6eZ5k"
 bot = Bot(token=BOT_TOKEN)
 
 dp = Dispatcher()
 
 @dp.message(Command('start'))
-async def start(message: types.Message):
+async def cmd_start(message: types.Message):
     await message.answer("Assalomu alekum!. Men echo botman")
 
 @dp.message(Command('help'))
-async def help(message: types.Message):
+async def cmd_help(message: types.Message):
     await message.answer("Men echo botman. \nSizga yuborgan har qanday xabaringizni qaytaraman.\n📝 Komandalar: \n/start - Botni qayta ishga tushirish \n/help - Yordam")
+
+@dp.message(Command('info'))
+async def cmd_info(message: types.Message):
+    await message.reply(
+        f"ℹ️ <b>Sizning ma'lumotlaringiz:</b>\n\n"
+        f"👤 Ism: {message.from_user.first_name}\n"
+        f"🆔 ID: {message.from_user.id}\n"
+        f"👤 Username: @{message.from_user.username or 'sizda_username_yoq'}",
+        parse_mode="HTML"
+    )
 
 @dp.message()
 async def echo(message: types.Message):
