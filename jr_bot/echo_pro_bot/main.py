977ef10868449805e10863523a2033d87dd27744
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-24 10:13:04 +0500

    echo pro

--- jr_bot/echo_pro_bot/main.py
+++ jr_bot/echo_pro_bot/main.py
@@ -1,15 +1,15 @@
 import asyncio
-import logging
 from aiogram import Bot, Dispatcher
 
 from config.settings import BOT_TOKEN
-from handlers import commands
+from handlers import commands, messages
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
     dp.include_router(commands.router)
+    dp.include_router(messages.router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
