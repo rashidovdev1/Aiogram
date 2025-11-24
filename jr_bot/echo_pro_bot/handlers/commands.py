82324079dc9652d09b7338b75fe9c3032aa551b0
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-24 10:33:34 +0500

    echo pro

--- jr_bot/echo_pro_bot/handlers/commands.py
+++ jr_bot/echo_pro_bot/handlers/commands.py
@@ -6,8 +6,21 @@ router = Router()
 
 @router.message(Command('start'))
 async def cmd_start(message: Message):
-    await message.answer('Salom!, Men echo botman')
+    await message.answer(
+        f"Assalomu alaykum {message.from_user.first_name}!\n\n"
+        f"Men oddiy Echo botman\n"
+    )
 
 @router.message(Command('help'))
 async def cmd_help(message: Message):
-    await message.answer('/start - Botni ishga tushirish \n/help - Yordam')
+    await message.answer("Komandalar: \n/start - Botni qayta ishga tushirish \n/info - ma'lumot ") #\n/weather - havo haqida
+
+@router.message(Command('info'))
+async def cmd_info(message: Message):
+    await message.reply(
+        f"ℹ️ <b>Sizning ma'lumotlaringiz:</b>\n\n"
+        f"🇺🇿 Ism: {message.from_user.first_name}\n"
+        f"🆔 ID: {message.from_user.id}\n"
+        f"👤 Username: @{message.from_user.username or 'sizda_username_yoq'}",
+        parse_mode="HTML"
+    )
