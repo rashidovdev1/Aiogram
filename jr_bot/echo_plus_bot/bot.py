216ecd8e2531b65e2628bcee5cc6a8d66f6056a8
rashidovdev <rozibekabdurashidov590@gmail.com>
2025-11-24 20:26:24 +0500

    echo protect

--- jr_bot/echo_plus_bot/bot.py
+++ jr_bot/echo_plus_bot/bot.py
@@ -11,11 +11,17 @@ dp = Dispatcher()
 
 @dp.message(Command('start'))
 async def cmd_start(message: types.Message):
-    await message.answer("Assalomu alekum!. Men echo botman")
+    await message.answer("Assalomu alekum!. Men echo botman",protect_content=True )
 
 @dp.message(Command('help'))
 async def cmd_help(message: types.Message):
-    await message.answer("Men echo botman. \nSizga yuborgan har qanday xabaringizni qaytaraman.\n📝 Komandalar: \n/start - Botni qayta ishga tushirish \n/info - malumot \n/weather - havo haqida")
+    await message.answer(""
+                         "Men echo botman. \nSizga yuborgan har qanday xabaringizni qaytaraman."
+                         "\n📝 Komandalar: "
+                         "\n/start - Botni qayta ishga tushirish \n/info - malumot "
+                         "\n/weather - havo haqida",
+                        protect_content = True
+    )
 
 @dp.message(Command('info'))
 async def cmd_info(message: types.Message):
@@ -24,19 +30,20 @@ async def cmd_info(message: types.Message):
         f"👤 Ism: {message.from_user.first_name}\n"
         f"🆔 ID: {message.from_user.id}\n"
         f"👤 Username: @{message.from_user.username or 'sizda_username_yoq'}",
+        protect_content = True,
         parse_mode="HTML"
     )
 @dp.message(Command('weather'))
 async def cmd_weather(message: types.Message):
-    await message.reply("🌤 Bugun ob-havo yaxshi!")
+    await message.reply("🌤 Bugun ob-havo yaxshi!",protect_content=True)
 
 @dp.message(lambda msg: msg.text and msg.text.lower() in ['salom', 'assalom', 'assalomu alaykum'])
 async def salom_txt(message: types.Message):
-    await message.reply("✋🏻Vaalaykum assalom! Qalaysiz?")
+    await message.reply("✋🏻Vaalaykum assalom! Qalaysiz?",protect_content=True)
 
 @dp.message(lambda msg: msg.text and msg.text.lower() in ['raxmat','rahmat', 'thanks', 'thank you'])
 async def raxmat_txt(message: types.Message):
-    await message.reply("Arzimaydi🤝")
+    await message.reply("Arzimaydi🤝",protect_content=True)
 
 @dp.message(F.text.regexp(r'^\d+$'))
 async def calculate_handler(message: types.Message):
@@ -46,12 +53,16 @@ async def calculate_handler(message: types.Message):
     await message.reply(
         f"🔢 Hisoblash:\n"
         f"✖️ {number} × 2 = <b>{result}</b>",
-        parse_mode="HTML"
+        protect_content=True,
+    parse_mode="HTML"
     )
 
 @dp.message()
 async def echo(message: types.Message):
-    await message.copy_to(chat_id=message.chat.id)
+    await message.copy_to(
+        chat_id=message.chat.id,
+        protect_content=True
+    )
 
 async def main():
     print('bot ishladi')
